8c03f4cd5a57f0a964f57fd6661b9d39ea34e784
Matthew Muresan <Matthew Muresan@mainwin7-pc>
2016-02-06 20:00:11 +0000

    added stop pulling/pushing.

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ import calendar
 import threading
 import xml.etree.cElementTree as cET
 
-writefilemode = "w"
+writefilemode = "a"
 getagency = "ttc"
-getroute = "511"
+getroute = "501"
 requestime = str(calendar.timegm(time.gmtime()))
+datawritefile = {}
+unwrittenvehicles = {}
+currdate = time.strftime("%d-%b-%Y")
+currtime = time.strftime("%H%Mh")
+rootdir = os.path.dirname(os.path.realpath(__file__))
+writedir = rootdir + "/write" 
 
 class windowThread (threading.Thread):
     def __init__(self, threadID, name, counter):
@@ -24,19 +30,11 @@ class windowThread (threading.Thread):
         self.sigterm = True
         return
 
-def main():
-    rootdir = os.path.dirname(os.path.realpath(__file__))
-    writedir = rootdir + "/write" 
-    currdate = time.strftime("%d-%b-%Y")
-    currtime = time.strftime("%H%Mh")
-        
+def main():        
     if not os.path.isdir(writedir):
         os.makedirs(writedir)
         
-    datawritefile = open(writedir + "/" + currdate + ".csv", writefilemode)
-    datawritefile.write("XML Data Parsed for " + currdate + "Starting at: " + currtime + "\n")
-    datawritefile = {}
-    unwrittenvehicles = {}
+    
     
     #extra thread that runs, checking if input has been made to the console. If so, it sets a variable to be True, indicating that the main loop should cleanup.
     thread1 = windowThread(1, "Thread-1", 1)
@@ -48,36 +46,39 @@ def main():
         requestime = str(calendar.timegm(time.gmtime()))
         event_count += 1
         #print("http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=" + getagency + "&r=" + getroute + "&t=" + requestime)
-        with urllib.request.urlopen("http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=" + getagency + "&r=" + getroute + "&t=" + requestime) as response:
-            xmlRESP = response.read()
-            root = cET.fromstring(xmlRESP)
-            for child in root:
-                if child.tag == "vehicle":
-                    try:
-                        if not child.attrib["id"] in unwrittenvehicles:
-                            append_data = {"lon":child.attrib["lon"], "lat":child.attrib["lat"], "posix":requestime}
-                            unwrittenvehicles.update({child.attrib["id"]:[append_data]})
-                        elif (child.attrib["lon"] != unwrittenvehicles[child.attrib["id"]][-1]["lon"]) and (child.attrib["lat"] != unwrittenvehicles[child.attrib["id"]][-1]["lat"]):
-                            append_data = {"lon":child.attrib["lon"], "lat":child.attrib["lat"], "posix":requestime}
-                            unwrittenvehicles[child.attrib["id"]].append(append_data)
-                    except:
-                        pass #bad programmatic practice, I know.
-            print(unwrittenvehicles)
+        try:
+            with urllib.request.urlopen("http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=" + getagency + "&r=" + getroute + "&t=" + requestime) as response:
+                xmlRESP = response.read()
+                root = cET.fromstring(xmlRESP)
+                for child in root:
+                    if child.tag == "vehicle":
+                        try:
+                            if not child.attrib["id"] in unwrittenvehicles:
+                                append_data = {"lon":child.attrib["lon"], "lat":child.attrib["lat"], "posix":requestime}
+                                unwrittenvehicles.update({child.attrib["id"]:[append_data]})
+                            elif (child.attrib["lon"] != unwrittenvehicles[child.attrib["id"]][-1]["lon"]) and (child.attrib["lat"] != unwrittenvehicles[child.attrib["id"]][-1]["lat"]):
+                                append_data = {"lon":child.attrib["lon"], "lat":child.attrib["lat"], "posix":requestime}
+                                unwrittenvehicles[child.attrib["id"]].append(append_data)
+                        except:
+                            pass #bad programmatic practice, I know.
+                print(unwrittenvehicles)
+        except:
+            print("Contact with electronic resource lost...") #well we've failed.
         
         #every so often, write the data that's been collected to file.    
         if event_count % 100 == 0:
             print("Flushing to file...")
                     
-            for id, elem in unwrittenvehicles.items():
-                if not id in datawritefile:
-                    newwritefile = open(writedir + "/" + currdate + "-" + id + ".csv", writefilemode)
-                    datawritefile.update({id:newwritefile})
-                    datawritefile[id].write("XML Data Parsed for " + currdate + "Starting at: " + currtime + "\n")
-                    datawritefile[id].write("POSIX,lon,lat\n")
+            for idtag, elem in unwrittenvehicles.items():
+                if not idtag in datawritefile:
+                    newwritefile = open(writedir + "/" + currdate + "-" + idtag + ".csv", writefilemode)
+                    datawritefile.update({idtag:newwritefile})
+                    datawritefile[idtag].write("XML Data Parsed for " + currdate + "Starting at: " + currtime + "\n")
+                    datawritefile[idtag].write("POSIX,lon,lat\n")
                     
                 while len(elem) > 1: #don't write the last item, save it for reference.
                     item = elem.pop(0)
-                    datawritefile[id].write(item["posix"] + "," + item["lon"] + "," + item["lat"] + "\n")
+                    datawritefile[idtag].write(item["posix"] + "," + item["lon"] + "," + item["lat"] + "\n")
         
         #check if the flag has been made, clean up by closing all open file handles.
         if (thread1.sigterm):
@@ -93,4 +94,24 @@ if __name__ == "__main__":
         
     except:
         print("Unhandled Error.")
+        if not datawritefile:
+            try:
+                print("Flushing to file...")        
+                for id, elem in unwrittenvehicles.items():
+                    if not id in datawritefile:
+                        newwritefile = open(writedir + "/" + currdate + "-" + id + ".csv", writefilemode)
+                        datawritefile.update({id:newwritefile})
+                        datawritefile[id].write("XML Data Parsed for " + currdate + "Starting at: " + currtime + "\n")
+                        datawritefile[id].write("POSIX,lon,lat\n")
+                        
+                    while len(elem) > 0: #don't write the last item, save it for reference.
+                        item = elem.pop(0)
+                        datawritefile[id].write(item["posix"] + "," + item["lon"] + "," + item["lat"] + "\n")
+                
+                print("Flushing files")
+                for activefile in datawritefile:
+                    datawritefile[activefile].close()
+            except:
+                print("Failed to flush!")
+            
         raise
---
+++ stop_loc_build.py
@@ -0,0 +1,89 @@
+#fetch XML of stops and convert to EXCEL.
+
+import os
+import time
+import urllib.request
+import calendar
+import xml.etree.cElementTree as cET
+
+writefilemode = "w"
+getagency = "ttc"
+getroute = "501"
+requestime = str(calendar.timegm(time.gmtime()))
+stopdata = []
+currdate = time.strftime("%d-%b-%Y")
+currtime = time.strftime("%H%Mh")
+rootdir = os.path.dirname(os.path.realpath(__file__))
+writedir = rootdir + "/write/auxdata" 
+rm = True
+
+def main():        
+    if not os.path.isdir(writedir):
+        os.makedirs(writedir)
+    
+    event_count = 0
+    requestime = str(calendar.timegm(time.gmtime()))
+    event_count += 1
+    print("http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=" + getagency + "&r=" + getroute)
+    try:
+        with urllib.request.urlopen("http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=" + getagency + "&r=" + getroute) as response:
+            xmlRESP = response.read()
+            root = cET.fromstring(xmlRESP)
+            for child in root:
+                #print(child.tag)
+                if child.tag == "route":
+                    for baby in child:
+                        if baby.tag == "stop":
+                            for attrib in baby.attrib:
+                                print(attrib)
+                            if "stopId" in baby.attrib:
+                                stopID = baby.attrib["stopId"] #stopID not guarunteed.
+                            else:
+                                stopID = ""
+                            stop = {"name":baby.attrib["title"], "stopID":stopID, "lat":baby.attrib["lat"], "lon":baby.attrib["lon"], "tag":baby.attrib["tag"]}
+                            stopdata.append(stop)
+                
+            print(stopdata)
+    except:
+        print("Contact with electronic resource lost...") #well we've failed.
+    
+    print("Flushing to file...")
+                
+    newwritefile = open(writedir + "/" + currdate + "-STOPS-" + getroute + ".csv", writefilemode)
+    newwritefile.write("XML Data Parsed for " + currdate + "Starting at: " + currtime + "\n")
+    newwritefile.write("title,lon,lat,stopid,tag\n")
+    for elem in stopdata:    
+        newwritefile.write(elem["name"] + "," + elem["lon"] + "," + elem["lat"] + "," + elem["stopID"] + "," + elem["tag"] + "\n")
+    newwritefile.close()
+    
+    
+    print("Successful termination")
+if __name__ == "__main__":
+    try:
+        main()
+        
+    except:
+        print("Unhandled Error.")
+        '''
+        if not stopdata:
+            try:
+                print("Flushing to file...")        
+                for id, elem in unwrittenvehicles.items():
+                    if not id in datawritefile:
+                        newwritefile = open(writedir + "/" + currdate + "-" + id + ".csv", writefilemode)
+                        datawritefile.update({id:newwritefile})
+                        datawritefile[id].write("XML Data Parsed for " + currdate + "Starting at: " + currtime + "\n")
+                        datawritefile[id].write("POSIX,lon,lat\n")
+                        
+                    while len(elem) > 0: #don't write the last item, save it for reference.
+                        item = elem.pop(0)
+                        datawritefile[id].write(item["posix"] + "," + item["lon"] + "," + item["lat"] + "\n")
+                
+                print("Flushing files")
+                for activefile in datawritefile:
+                    datawritefile[activefile].close()
+            except:
+                print("Failed to flush!")
+                '''
+            
+        raise
